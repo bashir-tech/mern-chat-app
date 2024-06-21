f59e38f164511fc73b6e6735d9e963a77fe43d8b
bashir-tech <baashiisse3@gmail.com>
2024-06-21 19:38:57 +0300

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mern-chat-app-master-b660.onrender.com", {
+			const socket = io("https://chat-app-zxxt.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
